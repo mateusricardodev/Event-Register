f35219ce20aa066fa4b263e5655826568d404dc9
mateusricardodev <mateus.ricardo761919@gmail.com>
2026-06-17 19:19:51 -0300

    docs: edit slq code

--- backend/prisma/migrations/20260615220227_etapa3_stock_and_email/migration.sql
+++ backend/prisma/migrations/20260615220227_etapa3_stock_and_email/migration.sql
@@ -1,11 +1,11 @@
 -- AlterEnum
-ALTER TYPE "RegistrationStatus" ADD VALUE 'overbooked';
+ALTER TYPE "RegistrationStatus" ADD VALUE IF NOT EXISTS 'overbooked';
 
 -- AlterTable
 ALTER TABLE "Payment" ALTER COLUMN "updatedAt" DROP DEFAULT;
 
 -- AlterTable
-ALTER TABLE "Registration" ADD COLUMN     "confirmationEmailSentAt" TIMESTAMP(3);
+ALTER TABLE "Registration" ADD COLUMN IF NOT EXISTS "confirmationEmailSentAt" TIMESTAMP(3);
 
 -- AlterTable
-ALTER TABLE "Ticket" ADD COLUMN     "sold" INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE "Ticket" ADD COLUMN IF NOT EXISTS "sold" INTEGER NOT NULL DEFAULT 0;
